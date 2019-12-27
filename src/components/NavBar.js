40723d1aa322609fbff66077c77adf05d444443a
NickCoombe1 <51348672+NickCoombe1@users.noreply.github.com>
2019-12-27 17:48:13 +1300

    intial setup + basic navigation

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import { InputBase, IconButton, Typography, Toolbar, AppBar } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({ //use this to format indivdual components
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1
+    }
+}));
+
+export default function NavBar() {
+    const classes = useStyles();
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        Contact List
+                </Typography>
+                    <InputBase
+                    placeholder="Search" />
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
