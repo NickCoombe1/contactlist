fe639af9e42a78726a563f885d16ac956c76a536
NickCoombe1 <51348672+NickCoombe1@users.noreply.github.com>
2019-12-29 12:06:35 +1300

    Cards display contacts correctly - need to format

--- src/components/DisplayContacts.js
+++ src/components/DisplayContacts.js
@@ -1,33 +1,82 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import { Card, CardActions, CardContent, Typography, Grid } from '@material-ui/core';
+import { makeStyles, withStyles } from "@material-ui/styles"
+
+const useStyles = makeStyles({
+    card: {
+        width: "100",
+        height: "100",
+
+    },
+    gridRoot: {
+        direction: "row",
+        justify: "center",
+        alignItems: "stretch",
+        flex: "row",
+
+    }
+
+});
 
 class DisplayContacts extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             contactInfo: []      //empty array to hold retrieved info    
         };
     }
 
-componentDidMount(){
-    let url = 'http://jsonplaceholder.typicode.com/users'
-    fetch(url)
-    .then(res =>  res.json())
-    .then(data => this.setState({ contactInfo: data})) //save response data into array intialised before
+    componentDidMount() {
+        let url = 'http://jsonplaceholder.typicode.com/users'
+        fetch(url)
+            .then(res => res.json()) //ERROR HANDLING
+            .then(data => this.setState({ contactInfo: data })) //save response data into array intialised before
 
-}
+    }
 
 
-render() {
-    const {contactInfo} = this.state; 
-    console.log("state", this.state.contactInfo);
-    return(
-        <div>
-            {contactInfo.name}
-        </div>
 
-    )
+    render() {
+        const { contactInfo } = this.state;
+        console.log("state", this.state.contactInfo); //delete this
+        const {classes} = this.props;
+        return (
+            <div className="home" style={{padding: 20}}>
+                <Grid spacing={2}>
+                    <Grid item xs={12}>
+                        <Grid container className= {classes.gridRoot} spacing={2}>
+                            {this.state.contactInfo.map(contact => {
+                                const { id, name, email, phone, company } = contact; //map data we need for cards 
+                                return (
+                                    <Grid key={id} item>
+                                        <Card className={classes.card}>
+                                            <CardContent>
+                                                <Typography variant="h5">
+                                                    {name}
+                                                </Typography>
+                                                <Typography variant="h6">
+                                                    {email}
+                                                </Typography>
+                                                <Typography variant="h6">
+                                                    {phone}
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                )
+                            })}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>
+
+        )
 
     }
 }
+DisplayContacts.propTypes = { //fixes invalid hook calls
+    classes: propTypes.object.isRequired,
+};
 
-export default DisplayContacts
+export default withStyles(useStyles)(DisplayContacts);
